f0f5f7afe8e49cfe87b13c75fa6dc14d81249717
Hiago Ioris <hiagoioris@gmail.com>
2025-04-04 00:39:31 +0000

    Nivel Intermediario

--- xadrez.c
+++ xadrez.c
@@ -4,19 +4,8 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
-int main() {
-    // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
-
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
-
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
-
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
-    int menu, torre=0, bispo=0, repeticao=1, rainha;
+int main(){
+    int menu, torre=0, bispo=0, repeticao=1, rainha, cavalo1, cavalo2=0;
     while (repeticao==1)//repeticao para facilitar a reproducao do jogo.
     {
        
@@ -27,7 +16,8 @@ int main() {
         printf("\t1. Torre\n");
         printf("\t2. Bispo\n");
         printf("\t3. Rainha\n");
-        printf("\t4. SAIR\n");
+        printf("\t4. Cavalo\n");
+        printf("\t5. SAIR\n");
         scanf("%d", &menu);
         fflush(stdin);
         
@@ -61,7 +51,24 @@ int main() {
             }
             break;
 
-        case 4:// finalizacao do loop e fim do jogo.
+        case 4://for e do while aninhados para controlar movimento em "L" do cavalo.
+            printf("Cavalo andou:\n");
+            for (cavalo1 = 0; cavalo1 < 1; cavalo1++)
+            {
+                do
+                {
+                    printf("BAIXO\n");
+                    cavalo2++;
+
+                } while (cavalo2<2);
+                
+                printf("ESQUERDA\n");
+                
+            }
+            cavalo2=0;
+            break;
+
+        case 5:// finalizacao do loop e fim do jogo.
             repeticao++;
             printf("FINALIZANDO JOGO");
     
@@ -71,9 +78,7 @@ int main() {
 
     }
 
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
+    
 
     // Nível Mestre - Funções Recursivas e Loops Aninhados
     // Sugestão: Substitua as movimentações das peças por funções recursivas.
